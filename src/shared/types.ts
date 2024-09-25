08a53448f7bed298f90949f16f309df7e29a3478
el jefecito <thobiloba33@gmail.com>
2024-09-25 17:47:16 +0100

    commit

---
+++ src/shared/types.ts
@@ -0,0 +1,20 @@
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus",
+}
+
+export default SelectedPage;
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
